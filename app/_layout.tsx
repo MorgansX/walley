a720d4b6a167a651e859d9174c1eaadd5060239b
arthurmorgan <echozilladev@outlook.com>
2025-05-04 13:31:59 +0300

    [setup] added libs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,5 @@
+
+import '../polyfills';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -6,23 +8,62 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+import {createPublicClient, formatEther, http} from "viem";
+import {mainnet} from "viem/chains";
+import {useState, useEffect} from "react";
+import {Button, Text} from "react-native";
+import '@walletconnect/react-native-compat';
+import { useWalletConnectModal, WalletConnectModal } from "@walletconnect/modal-react-native";
+
+const publicClient = createPublicClient({
+    chain: mainnet,
+    transport: http(),
+})
+
 export default function RootLayout() {
+    const { open, isConnected, provider, address: wcAddress } = useWalletConnectModal()
+    const [blockNumber, setBlockNumber] = useState(0n)
+    const [gasPrice, setGasPrice] = useState(0n)
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+    const projectId = process.env.EXPO_PUBLIC_CONNECTWALLET_ID;
+
+
+    useEffect(() => {
+        const getNetworkData = async () => {
+            const [blockNumber, gasPrice] = await Promise.all([
+                publicClient.getBlockNumber(),
+                publicClient.getGasPrice(),
+            ])
+
+            setBlockNumber(blockNumber)
+            setGasPrice(gasPrice)
+        }
+
+        getNetworkData();
+    }, [projectId]);
+
+    const providerMetadata = {
+        name: 'Example dApp',
+        description: 'Modern Example dApp from Callstack',
+        url: 'https://callstack.com/',
+        icons: ['https://example.com/'],
+        redirect: {
+            native: 'YOUR_APP_SCHEME://',
+            universal: 'YOUR_APP_UNIVERSAL_LINK.com'
+        }
+    }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+        <Text>Block number: {String(blockNumber)}</Text>
+        <Text>Gas price: {formatEther(gasPrice)} ETH </Text>
+
+        <Button title="Connect Wallet" onPress={() => open()}/>
+        <WalletConnectModal projectId={projectId} providerMetadata={providerMetadata}/>
       <StatusBar style="auto" />
     </ThemeProvider>
   );
